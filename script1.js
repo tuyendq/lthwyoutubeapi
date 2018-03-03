8572b1d8921935ff3c20c374b7e3994fcfa5e4a4
Tuyen Dang <tuyendq@gmail.com>
2018-03-03 23:06:09 +0700

    first commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+/* Codecademy: Youtube API - script1.js */
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
