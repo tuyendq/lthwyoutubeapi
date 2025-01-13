108da459672df5b9d07bd5c7856367611232af66
Tuyen Dang <tuyendq@gmail.com>
2025-01-13 23:32:34 +0700

    refactor json2csv.json

--- json2csv.py
+++ json2csv.py
@@ -1,29 +1,33 @@
-# Import and parse json file into csv file
+def main():
+    """Import and parse json file into csv file."""
 
-import json
-import csv
+    import json
+    import csv
 
-json_filename = 'MediaDowloaderList-MQL4Tutorial.json'
-with open('data\\'+json_filename, 'r') as file:
-    data = json.load(file)
+    json_filename = 'MediaDowloaderList-Better-Version.json'
+    with open('data\\'+json_filename, 'r') as file:
+        data = json.load(file)
 
-# print(type(data))  # data is a list
+    # print(type(data))  # data is a list
 
-# Get the keys (column names) from the dictionary
-fieldnames = data[0].keys()
-print(fieldnames)
+    # Get the keys (column names) from the dictionary
+    fieldnames = data[0].keys()
+    print(fieldnames)
 
-# Open a CSV file for writing
-with open(json_filename+'.csv', 'a', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Open a CSV file for writing
+    with open(json_filename+'.csv', 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    # Write the header (column names)
-    writer.writeheader()
+        # Write the header (column names)
+        writer.writeheader()
 
-    for el in data:
-        # print(type(el))  # el is a dictionnary
-        # print(len(el))
-        # Write the data rows
-        row = {key: el[key] for key in fieldnames}
-        writer.writerow(row)
+        for el in data:
+            # print(type(el))  # el is a dictionnary
+            # print(len(el))
+            # Write the data rows
+            row = {key: el[key] for key in fieldnames}
+            writer.writerow(row)
 
+if __name__ == "__main__":
+    print("Let's go")
+    main()
